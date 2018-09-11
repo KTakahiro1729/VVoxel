a82ba64d5e29f0de2d4af1ee6dfb909740bc428c
Allosteric <poet.takahiro1234@gmail.com>
2018-09-11 09:59:32 +0900

    speedup

--- VoxelVisual.py
+++ VoxelVisual.py
@@ -3,6 +3,7 @@ import itertools, math
 import numpy as np
 import os
 import datetime
+print(np.__version__)
 now = datetime.datetime.now
 from bpy_extras.io_utils import ExportHelper
 from bpy.props import (
@@ -30,23 +31,21 @@ bl_info = {
 
 def add_voxel(voxel):
     print("start")
-    start = datetime.datetime.now()
+    start = now()
     zerostart = np.zeros(np.add(voxel.shape,(2,2,2)))
     zerostart[1:-1,1:-1,1:-1] = voxel
     z_diff = np.diff(zerostart,axis=0)
     y_diff = np.diff(zerostart,axis=1)
     x_diff = np.diff(zerostart,axis=2)
 
-    vsz = add_planes(z_diff, "z")
-    vsy = add_planes(y_diff, "y")
-    vsx = add_planes(x_diff, "x")
-
-
-    vs = np.concatenate((vsz,vsy,vsx))
-    vs = np.array(vs)
-    fs = np.arange(len(vs)).reshape(len(vs)//4,4)
-    vs, fs = remove_doubles(vs,fs)
-    add_obj(vs,fs,"voxel")
+    z_vs = vs(z_diff,"z")
+    y_vs = vs(y_diff,"y")
+    x_vs = vs(x_diff,"x")
+    vs_ = np.concatenate((z_vs,y_vs,x_vs))
+    vs_ = np.fliplr(vs_)
+    fs = np.arange(len(vs_))
+    vs_, fs = remove_doubles(vs_,fs)
+    add_obj(vs_,fs.reshape(fs.size//4,4).tolist(),"voxel")
     print("took {0}secs".format((datetime.datetime.now() - start).total_seconds()))
 def add_obj(vs,fs,name="voxel"):
     mesh_data = bpy.data.meshes.new(name+"_mesh_data")
@@ -56,50 +55,53 @@ def add_obj(vs,fs,name="voxel"):
     scene = bpy.context.scene
     scene.objects.link(obj)
     obj.select = True
-def add_planes(normals_array, axis):
-    ns = normals_array.shape
-    z,y,x = np.mgrid[:ns[0],:ns[1],:ns[2]]
-    start = now()
-    vs = np.vectorize(add_plane)(x,y,z,axis,normals_array).flatten()
-    print(1,now()-start)
-    start = now()
-    vs = vs[vs!=np.array(None)]
-    vs = np.hstack(vs)
-    print(2,now()-start)
-    vs = vs.reshape(vs.size//3,3)
-    return vs
-def add_plane(x,y,z,axis,normal):
-    if normal == 0:
-        return None
-    if axis == "x":
-        offset = (0,-0.5,-0.5)
-    elif axis == "y":
-        offset = (-0.5, 0, -0.5)
-    elif axis == "z":
-        offset = (-0.5,-0.5,0)
-    loc = np.multiply((1,-1,-1), np.add((x,y,z),offset))
-    return add_face(loc, axis, normal)
-def parse_offset(offs):
 
-    dict_ = {"0":0,"+":0.5,"-":-0.5}
-    return [[dict_[i] for i in off] for off in offs.split(";")]
-def add_face(loc, axis, normal):
-    if axis == "x":
-        v_offs = parse_offset("0-+;0++;0+-;0--")
-    elif axis == "y":
-        v_offs = parse_offset("+0+;+0-;-0-;-0+")
-    elif axis == "z":
-        v_offs = parse_offset("+-0;++0;-+0;--0")
-    if normal == -1:
-        v_offs = v_offs[::-1]
-    vs = [add_vec(loc,v_off) for v_off in v_offs]
-    return list(np.hstack(vs))
-def add_vec(x,y):
-    return tuple(x[i]+y[i] for i in range(len(x)))
+def vs(diff,axis):
+    before = now()
+    loc=np.mgrid[:diff.shape[0],:diff.shape[1],:diff.shape[2]]
+    loc = np.swapaxes(np.rollaxis(loc,0,-1),-1,-2)
+    flat_loc = loc.flatten().reshape(loc.size//3,3)
+    del loc
+    flat_diff = diff.flatten()
+    del diff
+    skip_loc = flat_loc[flat_diff!=np.array(0)]
+    del flat_loc
+    skip_loc[:,:-1] = -skip_loc[:,:-1]
+
+    skip_diff = flat_diff[flat_diff != np.array(0)]
+    del flat_diff
+    minus_idx = np.where(skip_diff == -1)
+    del skip_diff
+    center_off = np.array({"z":[0.0,0.5,-0.5],"y":[0.5,0.0,-0.5], "x":[0.5,0.5,0.0]}[axis])
+    print(1,now()-before)
+    before = now()
+    centers = np.empty(skip_loc.shape)
+    for i in range(3):
+        centers[...,i] = skip_loc[...,i] + center_off[i]
+    result = np.empty(centers.shape[:-1]+(4,3))
+    print(2,now()-before)
+    before = now()
+    around_off ={'z': [[0, -0.5, 0.5], [0, 0.5, 0.5], [0, 0.5, -0.5], [0, -0.5, -0.5]],
+ 'y': [[0.5, 0, 0.5], [-0.5, 0, 0.5], [-0.5, 0, -0.5], [0.5, 0, -0.5]],
+ 'x': [[0.5, -0.5, 0], [0.5, 0.5, 0], [-0.5, 0.5, 0], [-0.5, -0.5, 0]]}[axis]
+    for i in range(4):
+        for j in range(3):
+            result[...,i,j] = around_off[i][j] + centers[...,j]
+    print(3,now()-before)
+    before = now()
+    result[minus_idx] = np.flip(result[minus_idx],1)
+    result = result.flatten().reshape(result.size//3,3)
+    print(4,now()-before)
+    before = now()
+
+    return result
 def remove_doubles(vs, fs):
+    print("number of verteces: ",len(vs))
+    start = now()
     usedvs_index = dict([])
     new_vs = []
     old_new = []
+
     for i, v in enumerate(vs):
         v = tuple(v)
         if v in usedvs_index.keys():
@@ -108,8 +110,9 @@ def remove_doubles(vs, fs):
             usedvs_index[v] = i
             old_new.append(len(new_vs))
             new_vs.append(v)
-    new_fs = [tuple(old_new[v] for v in f) for f in fs]
-
+    new_fs = np.vectorize(lambda i:old_new[i])(fs)
+    print("number of verteces: ",len(new_vs))
+    print("remove doubles took: ",now()-start)
     return new_vs, new_fs
 
 
